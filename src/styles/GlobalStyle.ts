6b1686fddd3912db49d09205f85483a9bbf695ab
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-03 03:52:07 +0900

    feat: 전역스타일 'hidden' 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -87,6 +87,18 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     cursor: pointer;
   }
+
+  .hide {
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+}
 `;
 
 export default GlobalStyle;
