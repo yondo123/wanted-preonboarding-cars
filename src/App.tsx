34b24d23647269e711062aa7299e7622994e18a9
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-03 03:54:16 +0900

    feat: Router, Layout 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import BackIcon from './components/BackIcon';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import { HeaderContext } from './contexts/HeaderContext';
+import Layout from './components/Layout';
+import Cars from './pages/Cars';
 
 function App() {
   return (
-    <div>
-      <h2>hello React</h2>
-      <BackIcon size={24} />
-    </div>
+    <HeaderContext>
+      <BrowserRouter>
+        <Content>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Cars />} />
+            </Route>
+          </Routes>
+        </Content>
+      </BrowserRouter>
+    </HeaderContext>
   );
 }
+
+const Content = styled.div`
+  color: ${({ theme }) => theme.black};
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.border};
+  min-width: 360px;
+  max-width: 450px;
+  height: 100vh;
+`;
 
 export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,17 @@
-function Layout() {
-  <>
-    <h2>asdasd</h2>
-  </>;
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Category from './Category';
+
+function Layout(): React.ReactElement {
+  return (
+    <>
+      <Header />
+      <main>
+        <Category />
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default Layout;
