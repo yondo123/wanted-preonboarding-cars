039f903605c04859e709f0c855cf59a89a2ec6fd
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2022-11-02 17:48:57 +0900

    test: icon 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import BackIcon from './components/BackIcon';
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <h2>hello React</h2>
+      <BackIcon size={24} />
+    </div>
+  );
 }
 
 export default App;
