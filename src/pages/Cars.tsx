c78a07663c2ba5ef94dc41ac1b63202123585db6
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-11-03 03:54:38 +0900

    add: 차량리스트 페이지 UI

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -1,3 +1,17 @@
-function Cars() {}
+import React from 'react';
+import Car from '../components/Car';
+
+function Cars(): React.ReactElement {
+  return (
+    <ul>
+      <Car />
+      <Car />
+      <Car />
+      <Car />
+      <Car />
+      <Car />
+    </ul>
+  );
+}
 
 export default Cars;
