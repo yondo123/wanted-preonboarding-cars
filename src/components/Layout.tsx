b5b1acd4e8a9350e003593d57449755c54a605f2
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2022-11-03 20:46:31 +0900

    fix: 공통헤더 위치 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Category from './Category';
 
 function Layout(): React.ReactElement {
   return (
     <>
       <Header />
       <main>
-        <Category />
         <Outlet />
       </main>
     </>
